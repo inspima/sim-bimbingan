62683c474457d8c6f3c908ddf461f362828eb0aa
Nambi Sembilu <nambisembilu@gmail.com>
2021-03-20 05:17:25 +0700

    Update jadwal_kalender.php

--- application/views/frontend/dashboard/jadwal_kalender.php
+++ application/views/frontend/dashboard/jadwal_kalender.php
@@ -23,7 +23,7 @@
 				<div class="modal-header bg-gray">
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span></button>
-					<h3 class="modal-title">Info Ujian</h3>
+					<h3 class="modal-title">Info detail</h3>
 				</div>
 				<div class="modal-body" id="modal-ujian-content">
 				</div>
