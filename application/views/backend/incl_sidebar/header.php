800de3b07645a8fcda375b8eaa9d7dc28a04b9d4
Nambi Sembilu <nambisembilu@gmail.com>
2021-04-01 14:48:41 +0700

    Update > UI Warna Button Profile dan Signout

--- application/views/backend/incl_sidebar/header.php
+++ application/views/backend/incl_sidebar/header.php
@@ -48,10 +48,10 @@
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left">
-                                <a href="<?php echo base_url() ?>profile" class="btn btn-default btn-flat">Profile</a>
+                                <a href="<?php echo base_url() ?>profile" class="btn btn-primary">Profile</a>
                             </div>
                             <div class="pull-right">
-                                <a href="<?php echo base_url() ?>logout" class="btn btn-default btn-flat">Sign out</a>
+                                <a href="<?php echo base_url() ?>logout" class="btn btn-warning">Sign out</a>
                             </div>
                         </li>
                     </ul>
