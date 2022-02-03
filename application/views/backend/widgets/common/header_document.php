8e50e20126930f8931bf54f453cf1a403941f42e
Nambi Sembilu <nambisembilu@gmail.com>
2022-02-03 07:44:48 +0700

    Update kop surat

--- application/views/backend/widgets/common/header_document.php
+++ application/views/backend/widgets/common/header_document.php
@@ -1,11 +1,10 @@
 <table align="center" width="100%" border="0">
 	<tbody>
 	<tr>
-		<td width="10%"><img src="assets/backend/cetak/logo.png" width="100px"></td>
+		<td width="10%"><img src="assets/backend/cetak/logo_unair.png" width="100px"></td>
 		<td width="90%" align="center">
 			<strong>
 				<p style="font-size:17px;margin-bottom: 0px;">
-					<?= strtoupper($this->setting->get_value('kementrian_txt')) ?><br>
 					<?= strtoupper($this->setting->get_value('universitas_txt')) ?><br>
 					<?= strtoupper($this->setting->get_value('universitas_fakultas_txt')) ?><br>
 				</p>
