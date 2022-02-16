0574bb742fb9eb25d26b97871a22de60ba35c2d6
Nambi Sembilu <nambisembilu@gmail.com>
2022-02-16 16:26:01 +0700

    Fix penguji baris kedua

--- application/views/backend/widgets/disertasi/form_persetujuan_penguji.php
+++ application/views/backend/widgets/disertasi/form_persetujuan_penguji.php
@@ -4,21 +4,23 @@
 			if ($disertasi->status_kualifikasi >= STATUS_DISERTASI_KUALIFIKASI_SETUJUI_KPS) {
 				?>
 				<?php echo form_open('dosen/disertasi/permintaan/penguji/setujui') ?>
-				<?php echo formtext('hidden', 'hand', 'center19', 'required') ?>
-				<button class="btn btn-xs btn-success"><i class="fa fa-check"></i> Proses Setujui</button><br/>
 				<?php echo formtext('hidden', 'id_penguji', $penguji->id_penguji, 'required') ?>
 				<?php echo formtext('hidden', 'id_disertasi', $id_disertasi, 'required') ?>
 				<?php echo formtext('hidden', 'id_ujian', $jadwal->id_ujian, 'required') ?>
+				<?php echo formtext('hidden', 'hand', 'center19', 'required') ?>
+				<button class="btn btn-xs btn-success"><i class="fa fa-check"></i> Proses Setujui</button><br/>
+				<?php echo form_close() ?>
 				<?php
 			}
 		} else {
 			?>
 			<?php echo form_open('dosen/disertasi/permintaan/penguji/setujui') ?>
 			<?php echo formtext('hidden', 'hand', 'center19', 'required') ?>
-			<button class="btn btn-xs btn-success"><i class="fa fa-check"></i> Proses Setujui</button><br/>
 			<?php echo formtext('hidden', 'id_penguji', $penguji->id_penguji, 'required') ?>
 			<?php echo formtext('hidden', 'id_disertasi', $id_disertasi, 'required') ?>
 			<?php echo formtext('hidden', 'id_ujian', $jadwal->id_ujian, 'required') ?>
+			<button class="btn btn-xs btn-success"><i class="fa fa-check"></i> Proses Setujui</button><br/>
+			<?php echo form_close() ?>
 			<?php
 		}
 
